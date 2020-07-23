ae3b2f43d7831e1ab6454ce6d6b5fca67308d511
Asher Hoeveler <Crinfarr@users.noreply.github.com>
2020-07-22 21:29:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
 
 //required functions
-const http = require('http');
 const Twit = require('Twit');
 const config = require('./config');
-
-//for http
-const hostname = '127.0.0.1';
-const port = 3000;
 
 //for twit
 var T = new Twit (config);
 
-//http init
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('hello world!');
-});
+function testtweet() {
+    T.post('statuses/update', {status: 'this is a test tweet from a bad idea by @rb_farr', function(err, data, response) {
+        console.log(data);
+    }});
+}
 
-//http complete
-server.listen(port, hostname, () => {
-    console.log(`server running at http://${hostname}:${port}`);
-
-});
+testtweet();
