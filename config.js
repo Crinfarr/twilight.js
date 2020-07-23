419a2168b809174f1c18b3ba8d4f4fbe905d832d
Asher Hoeveler <Crinfarr@users.noreply.github.com>
2020-07-22 19:45:58 -0500

    Add basic start

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    consumer_key:'Twitter key goes here',
+    consumer_secret:'Twitter secret goes here',
+    access_token:'Access token goes here',
+    access_token_secret:'Secret token goes here'
+
+}
