09edddf4783947b8ed0812e853dda9417c812489
Asher Hoeveler <Crinfarr@users.noreply.github.com>
2020-07-22 20:40:56 -0500

    move config schema to readme.md

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    consumer_key:'Twitter key goes here',
-    consumer_secret:'Twitter secret goes here',
-    access_token:'Access token goes here',
-    access_token_secret:'Secret token goes here'
-
-}
