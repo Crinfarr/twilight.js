0221b6c5ce49704ba3bb882a972414fb2c30b829
Asher Hoeveler <Crinfarr@users.noreply.github.com>
2020-09-16 15:35:32 -0500

    Update MH lib version

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
+const {MH, Discovery} = require('magic-home');
 
-//required functions
-const Twit = require('Twit');
-const config = require('./config');
-
-//for twit
-var T = new Twit (config);
-
-function testtweet() {
-    T.post('statuses/update', {status: 'this is a test tweet from a bad idea by @rb_farr', function(err, data, response) {
-        console.log(data);
-    }});
-}
-
-testtweet();
+let discover = new Discovery();
+discover.scan(500).then(devices => {
+    console.log(devices);
+});
