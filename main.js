983efb74d8b573ee13fb705eaf3da7d33b782a18
Asher Hoeveler <Crinfarr@users.noreply.github.com>
2020-09-16 21:04:01 -0500

    begin work on light finder

--- main.js
+++ main.js
@@ -1,31 +1,47 @@
 const {MH, Discovery, Control} = require('magic-home');
+const prompt = require('prompt');
 
 let discover = new Discovery();
+controller = new Control();
+
+let discovered;
 
 var scan = function() {
     return new Promise(resolve => {
-        discover.scan(500).then(devices => {
+        discover.scan(1000).then(devices => {
             console.log("returned values")
             resolve(devices);
         });
     });
 }
 
+var layout = Object;
+
+//var countdown = async function(from) {
+//    for (x=0; x<=from, x++;) {
+//        setTimeout(() => {
+//            console.log(from-x+" seconds remaining.");
+//        }, 1000)
+//    }
+//}
+
 var main  = async function() {
-    const discovered = await scan();
+    discovered = await scan();
     console.log("wait finished");
     console.log(discovered);
-    //console.log(discovered.length);//should equal 4 or 5
-    //console.log(discovered[0]);
-    //for (i = 0; i <= discovered.length; i++) {
-    //    console.log(discovered[i]);
-    //}
-    //test secondary version:
     i = 1;
     discovered.forEach(element => {
-        
         console.log("Light "+i+" address:"+element.address);
         i++;
+        controller = new Control(element.address);
+        controller.turnOff();
+    });
+    i=1;
+    discovered.forEach(element => {
+        controller = new Control(element.address);
+        controller.turnOn()
+        //console.log("THIS IS LIGHT NUMBER "+i+"!\nWrite this down.\n");
     });
 }
+prompt.start();
 main();
