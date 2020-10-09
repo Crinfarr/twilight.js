279655d13145fa4f7937ac59d927915e84a02242
Crinfarr <31548109+Crinfarr@users.noreply.github.com>
2020-10-08 21:35:45 -0600

    FINALLY FIX DOWNREZ!

--- bot.js
+++ bot.js
@@ -12,62 +12,67 @@ client.once('ready', () => {
     console.log(`Successfully logged in as ${client.user.tag}!`)
 });
 
-client.on('message', msg => {
-    if (msg.content.toLowerCase().includes("daddy") && config.daddymode) {
-        msg.channel.send(":weary:");
-    }
+async function getImg(url, path) {
+    return new Promise(resolve => {
+        request.get(url, function (error, response, body) {
+            fs.writeFileSync(path, Buffer.from(body));
+            resolve();
+        });
+    })
+}
 
-    if (!msg.content.startsWith(config.discord.prefix) || msg.author.bot) return;//don't respond to messages not calling it
+client.on('message', async msg => {
+        if (msg.content.toLowerCase().includes("daddy") && config.daddymode) {
+            msg.channel.send(":weary:");
+        }
 
-    console.log(`${msg.author.username}: ${msg.content} [in: ${msg.guild.name}]`);//log command call
-    
-    args = msg.content.slice(config.discord.prefix.length).trim().split(' ');//get command and arguments
-    command = args.shift().toLowerCase();//take command, leave arguments
-    
-    switch(command) {
-        case('downrez'):
-            //check to see if both arguments are specified:
-            if (args.length != 2) {
-                return msg.channel.send("Usage: downrez [width] [height]");
-            }
-            //if both args are numbers
-            else if (msg.attachments.size == 1) {//if there's only one attachment
-                console.log(msg.attachments.array()[0].url);
-                try {
-                    request.get(msg.attachments.array()[0].url, function(error, response, body) {
-                        if (!error && response.statusCode == 200) {//if image existed
-                            image = Buffer.from(body);//import buffer into image var
-                            var imgurl = msg.attachments.array()[0].url.split('.');//get image url
-                            var extension = imgurl[imgurl.length-1];//use image url to get extension
-                            if (extension == ("jpg" || "png" || "bmp" || "tiff" || "gif")) {//check if extension is valid like you
-                                
-                            }
-                            else {
-                                msg.channel.send(':x: Invalid attachment type!');
-                            }
-                        }
-                    });
-                    //console.log(image);
+        if (!msg.content.startsWith(config.discord.prefix) || msg.author.bot) return; //don't respond to messages not calling it
+        console.log(`${msg.author.username}: ${msg.content} [in: ${msg.guild.name}]`); //log command call
+
+        args = msg.content.slice(config.discord.prefix.length).trim().split(' '); //get command and arguments
+        command = args.shift().toLowerCase(); //take command, leave arguments
+
+        switch (command) {
+            case ('downrez'):
+                //check to see if both arguments are specified:
+                if (args.length != 2) {
+                    return msg.channel.send("Usage: downrez [width] [height]");
                 }
-                catch (err){
-                    throw(err);
+
+                //if both args are numbers
+                else if (msg.attachments.size == 1) { //if there's only one attachment
+                    try {
+                        console.log(msg.attachments.array()[0].url);
+                        var imgurl = msg.attachments.array()[0].url; //get image url
+                        var extension = imgurl.split(".")[imgurl.split(".").length - 1]; //use image url to get extension
+                        var path = `./tmpimages/image.${extension}`;
+                        var rspath = `./rstmpimages/rsimage.${extension}`;
+                        getImg(imgurl, path).then(function() {
+                            Jimp.read(path).then(img => {
+                                img.contain(parseInt(args[0]), parseInt(args[1])).autocrop().write(rspath);
+                            });
+                        })
+                        
+                    }
+                    catch(err) {
+                        msg.channel.send(`:x: JS Error: ${err}`);
+                    }
                 }
-            }
-            else if (msg.attachments.size > 1) {
-                msg.channel.send(':x: Too many attachments!');
-                
-            }
-            else if (msg.attachments.size < 1) {
-                msg.channel.send(':x: no valid attachment!');
-                
-            }
-            else {
-                msg.channel.send(':x: Unknown error!');
-                
-            }
-        default:
-            break;
-    }
-});
+                else if (msg.attachments.size > 1) {
+                    msg.channel.send(':x: Too many attachments!');
+
+                }
+                else if (msg.attachments.size < 1) {
+                    msg.channel.send(':x: no valid attachment!');
+
+                }
+                else {
+                    msg.channel.send(':x: Unknown error!');
+
+                }
+            default:
+                break;
+        }
+    });
 
 client.login(config.discord.token);
